5927173f3cfd5d0d0f28e71b7499bb28302a9f17
davidebbo <david.ebbo@microsoft.com>
2013-11-23 16:36:41 -0800

    Initial commit

---
+++ RpnCalc/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RpnCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack<int>();
+            for (; ; )
+            {
+                try
+                {
+                    Console.Write("> ");
+                    string line = Console.ReadLine();
+                    if (line == null) break;
+
+                    foreach (string token in line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries))
+                    {
+                        if (token == "+") stack.Push(stack.Pop() + stack.Pop());
+                        else if (token == "-") stack.Push(-(stack.Pop() - stack.Pop()));
+                        else if (token == "*") stack.Push(stack.Pop() * stack.Pop());
+                        else stack.Push(Int32.Parse(token));
+                    }
+
+                    Console.WriteLine(stack.Peek() + "\n");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+    }
+}
